eb2bc69ab38ffad19e7c591b48710e9076a5bc7a
wkmeijer <wilko94@gmail.com>
2014-10-29 18:41:10 +0100

    minor bug fix in interpolate; frequentie nu makkelijk af te lezen met goede grafiek; get_hertz werkt nog niet helemaal naar behoren

--- fft_etc.py
+++ fft_etc.py
@@ -6,6 +6,11 @@ import Leap
 import numpy as np
 import matplotlib.pyplot as plt
 
+# fft from scipy
+import scipy
+import scipy.fftpack
+import pylab
+import math
 
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 from numpy import fft
@@ -29,7 +34,7 @@ class SampleListener(Leap.Listener):
  #       print "Frame id: %d  timestamp: %d  hand position: %f  finger position: %f"% (
  #               frame.id, frame.timestamp, hand.palm_position.y, hand.fingers[1].joint_position(3).y)
         
-        print "Frame id: %d  timestamp: %d  relative finger position: %f"% (
+        print "Frame id: %d  timestamp: %d  hand position: %f"% (
                 frame.id, frame.timestamp, hand.palm_position.y) #- hand.fingers[1].joint_position(3).y)        
         global x,y# ,y2,count
         # count= count+1
@@ -55,23 +60,25 @@ def main():
         # Remove the sample listener when done
         controller.remove_listener(listener)
         
-        x[:]= [t - x[0] for t in x]
+        # time data starts from 0 and is set to seconds
+        x[:] = [(t - x[0])/1e6 for t in x]
         
-        plt.figure(1)
-        plt.subplot(211)
-        plt.plot(x,y)
-        plt.grid()
+        # plt.figure(3)
+        # plt.subplot(211)
+        # plt.plot(x,y)
+        # plt.grid()
                
 
         xf, yf = fourier_transform(x,y)
         
         print(xf.min(), xf.max())
         
-        print("total time: %f" % (x[len(x)-1]/1000000.0))
+        print("total time: %f" % (x[len(x)-1]))
         print("hertz: %f" % get_hertz(xf,yf))
         
-        plt.subplot(212)
-        plt.plot(xf,np.abs(yf))
+        # plot the fft with reasonable amount of hertz(1+ Hz); x axis: hertz
+        # plt.subplot(212)
+        # plt.plot(xf*100,np.abs(yf))
         
         plt.grid()
         
@@ -85,18 +92,25 @@ def main():
         xif,yif = fourier_transform(xi,yi)       
         print(xif.min(), xif.max())
         
-        print("total time: %f" % (xi[len(xi)-1]/1000000.0))
+        print("total time: %f" % (xi[len(xi)-1]))
         print("hertz: %f" % get_hertz(xif,yif))
         
+        # print("x: ", x)
+        # print("xif: ",xif)
+        # print("yif: ",yif)
+        # fourier_scipy(xi,yi)
         
-        
-        plt.figure(3)
-        plt.subplot(231)
+        plt.figure(2)
+        plt.subplot(211)
         plt.plot(xi,yi)
+        plt.xlabel("time(seconds)")
+        plt.ylabel("y-value(handpalm)")
         plt.grid()
         
-        plt.subplot(232)
-        plt.plot(xif,np.abs(yif))
+        # plot the fft with reasonable amount of hertz(1+ Hz); x axis: hertz
+        plt.subplot(212)
+        plt.plot(xif*100,np.abs(yif))
+        plt.xlabel('Hertz')
         plt.grid()
         plt.show()
         
@@ -108,6 +122,15 @@ def fourier_transform(x,y):
     # frequency of the data
     xf = fft.fftfreq(len(yf))
     
+    # print("xf before: ", xf)
+    mask = (xf > 0.01) & (xf <= .10) # show this frequency range 1Hz to 10Hz
+    xf = xf[mask]
+    # print("xf after: ", xf)
+    
+    # print("yf before: ", yf)
+    yf = yf[mask]
+    # print("yf after: ", yf)
+    
     return xf,yf
     
     
@@ -115,12 +138,12 @@ def fourier_transform(x,y):
 def get_hertz(xf,yf):
     ayf = np.abs(yf)**2
     # find the max argument of ayf except the first one
-    idx = np.argmax(ayf[1:])
+    idx = np.argmax(ayf)
     # find the frequency in the processed timestamps
     freq = xf[idx]
     print("freq: %f" % freq)
     # framerate is the total amount of frames divided by the total time in seconds
-    frate = len(yf)/(x[len(x)-1]/1000000.0)
+    frate = len(y)/(x[len(x)-1])
     print("frate: %f" % frate)
     
     hertz = abs(freq*frate)
@@ -131,22 +154,34 @@ def interpolate(x,y):
     yi = interp1d(x,y)
     yi2 = interp1d(x,y, kind ='cubic')
     
-    # time in seconds
-    time = x[len(x)-1]/1000000
+    # total datapoints at 100 frames/s (rounded up)
+    points = int(math.ceil(x[len(x)-1]*100))
     
-    xi = np.linspace(x[0],x[len(x)-1],time*100)   
+    xi = np.linspace(x[0],x[len(x)-1],points)   
     
-    # plt.figure(2)
-    # plt.plot(x,y,'o', xi, yi(xi), '-', xi, yi2(xi),'--')
-    # plt.legend(['data', 'linear', 'cubic'], loc ='best')
-    # plt.grid()
+    plt.figure(1)
+    plt.plot(x,y,'o', xi, yi(xi), '-', xi, yi2(xi),'--')
+    plt.xlabel("time(seconds)")
+    plt.ylabel("y-value(handpalm)")
+    plt.legend(['data', 'linear', 'cubic'], loc ='best')
+    plt.grid()
     
     ynew = []
-    for x in range(0,time*100):
+    for x in range(0,points):
         ynew.append(yi(xi[x]).item())      
         
     return xi,ynew
 
+    # alternative fft function
+def fourier_scipy(x,y):
+    yf = abs(scipy.fft(y))
+    xf = scipy.fftpack.fftfreq(len(y), x[1] - x[0])
+    
+    pylab.subplot(211)
+    pylab.plot(x,y)
+    pylab.subplot(212)
+    pylab.plot(xf,20*scipy.log10(yf),'x')
+    pylab.show()
 
 if __name__ == "__main__":
     main()
